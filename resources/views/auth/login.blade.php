daecc2d420d2970bc52658c379bdc337ab92fae3
Mike Shu <heliax44@gmail.com>
2021-03-20 11:06:29 +0400

    feat: добавлена локализация

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('Log In') }}
                 </x-button>
             </div>
         </form>
