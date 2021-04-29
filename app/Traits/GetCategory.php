f64e86c6d6765c7507da0c05c222aaf6cbbf31ac
Mike Shu <heliax44@gmail.com>
2021-04-29 22:32:29 +0400

    feat: добавлены трейты "GetImage" и "GetCategory"

---
+++ app/Traits/GetCategory.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Traits;
+
+use App\Exceptions\NotFoundException;
+use App\Models\Category;
+use App\Services\CommonTools;
+use App\Services\Errors;
+use Cache;
+
+trait GetCategory
+{
+    /**
+     * @param  int   $id
+     * @param  bool  $trashed      Если передать "true", то метод вернет даже архивную категорию.
+     * @param  int   $callerDepth  Смотри: CommonTools::getCaller()
+     *
+     * @return Category
+     * @throws NotFoundException
+     */
+    protected function getCategory(
+        int $id,
+        bool $trashed = true,
+        int $callerDepth = 1
+    ): Category {
+
+        $category = $trashed
+            ? Category::withTrashed()->find($id)
+            : Category::find($id);
+
+        if (empty($category)) {
+
+            $caller = CommonTools::getCaller($callerDepth);
+
+            throw new NotFoundException(
+                "The category #{$id} was not found: {$caller}",
+                Errors::NOT_FOUND,
+            );
+
+        }
+
+        return $category;
+    }
+
+    /**
+     * @param  int   $id
+     * @param  int   $ttl          Время жизни кеша, сек.
+     * @param  bool  $trashed      Если передать "true", то метод вернет даже архивную категорию.
+     * @param  int   $callerDepth  Смотри: CommonTools::getCaller()
+     *
+     * @return Category
+     * @throws NotFoundException
+     */
+    protected function getCategoryCached(
+        int $id,
+        int $ttl = 60,
+        bool $trashed = true,
+        int $callerDepth = 1
+    ): Category {
+
+        $cacheKey = md5(__METHOD__.$id);
+        $category = Cache::get($cacheKey);
+
+        if (empty($category)) {
+
+            $category = $this->getCategory($id, $trashed, $callerDepth);
+            Cache::put($cacheKey, $category, $ttl);
+
+        }
+
+        return $category;
+    }
+}
---
+++ app/Traits/GetImage.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Traits;
+
+use App\Exceptions\NotFoundException;
+use App\Models\Image;
+use App\Services\CommonTools;
+use App\Services\Errors;
+use Cache;
+
+trait GetImage
+{
+    /**
+     * Возвращает изображение, предварительно проверив его существование.
+     *
+     * @param  int   $id
+     * @param  bool  $trashed      Если передать "true", то метод вернет даже архивное изображение.
+     * @param  int   $callerDepth  Смотри: CommonTools::getCaller()
+     *
+     * @return Image
+     * @throws NotFoundException
+     * @see CommonTools::getCaller()
+     */
+    protected function getImage(
+        int $id,
+        bool $trashed = true,
+        int $callerDepth = 1
+    ): Image {
+
+        $image = $trashed
+            ? Image::withTrashed()->find($id)
+            : Image::find($id);
+
+        if (empty($image)) {
+
+            $caller = CommonTools::getCaller($callerDepth);
+
+            throw new NotFoundException(
+                "The image #{$id} was not found: {$caller}",
+                Errors::NOT_FOUND,
+            );
+
+        }
+
+        return $image;
+    }
+
+    /**
+     * Возвращает кешированное изображение, предварительно проверив его существование.
+     *
+     * @param  int   $id
+     * @param  int   $ttl          Время жизни кеша, сек.
+     * @param  bool  $trashed      Если передать "true", то метод вернет даже архивное изображение.
+     * @param  int   $callerDepth  Смотри: CommonTools::getCaller()
+     *
+     * @return Image
+     * @throws NotFoundException
+     */
+    protected function getImageCached(
+        int $id,
+        int $ttl = 60,
+        bool $trashed = true,
+        int $callerDepth = 1
+    ): Image {
+
+        $cacheKey = md5(__METHOD__.$id);
+        $image = Cache::get($cacheKey);
+
+        if (empty($image)) {
+
+            $image = $this->getImage($id, $trashed, $callerDepth);
+            Cache::put($cacheKey, $image, $ttl);
+
+        }
+
+        return $image;
+    }
+}
